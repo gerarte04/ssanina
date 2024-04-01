a6f5b2f0bb9e3b69f7d37a58b7f5642d3b6a5ca7
gerarte04 <gerarte04@gmail.com>
2024-04-01 12:10:20 +0300

    added mz07 in class

--- mz07/mz07-1.cpp
+++ mz07/mz07-1.cpp
@@ -2,4 +2,5 @@ class Figure
 {
 public:
     virtual double get_square() const = 0;
+    virtual ~Figure() { }
 };
---
+++ mz07/mz07-4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+namespace Game {
+    template <typename T>
+    class Coord
+    {
+    public:
+        typedef T value_type;
+        value_type row;
+        value_type col;
+        Coord(value_type r = 0, value_type c = 0) : row(r), col(c) { }
+    };
+
+    template <typename value_type>
+    value_type diff(value_type a, value_type b)
+    {
+        return (a > b) ? a - b : b - a;
+    }
+
+    template <typename T>
+    typename Coord<T>::value_type dist(Coord<T> range, Coord<T> a, Coord<T> b)
+    {
+        typename Coord<T>::value_type diff_x = diff(a.col, b.col);
+
+        if ((a.col & 1 && a.row < b.row) || (!(a.col & 1) && a.row > b.row)) {
+            return diff_x + diff(a.row, b.row) - diff_x / 2;
+        } else if ((!(a.col & 1) && a.row < b.row) || (a.col & 1 && a.row > b.row)) {
+            return diff_x + diff(a.row, b.row) - (diff_x + 1) / 2;
+        }
+
+        return diff_x;
+    }
+}
+
+int main()
+{
+    using namespace Game;
+    Coord<int> c1(4, 0), c2(5, 4), range(10, 10);
+    std::cout << dist(range, c1, c2) << std::endl;
+}
